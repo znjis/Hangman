d127caf3e282337eed62a7561f8ffe5d21aeea77
Ilyes Chikh <ilchi1@ulaval.ca>
2022-06-30 21:22:54 -0400

    Finished -bugs

--- hangman.py
+++ hangman.py
@@ -7,12 +7,17 @@ class GameError(Exception):
     def __str__(self) -> str:
         return super().__str__()
 
-class hangman():
+class Hangman():
     def __init__(self, mot):
         self.mot = mot
-        print(self)  # dessine le hangman
-        
-        
+        self.guesses = [[], []]  # right 0 and wrong 1
+        self.lettres = []
+        for _ in self.mot:  # for each letter in the word
+                    self.lettres += [" _"]
+
+    def __getitem__(self, indice):
+        return self.guesses[indice]
+    
     def __str__(self):
         # a quoi est-ce que je veux que l'affichage ressemble?
         # des tirets qui definissent ou se situront les lettres
@@ -21,7 +26,6 @@ class hangman():
 
         # devrais-je dessiner le bonhomme?
             # comment dessiner le bonhomme dans un tel cas?
-        
         L1 = [" ----\n"]
         L2 = ["|    |\n"]
         L3 = ["|"]
@@ -31,55 +35,34 @@ class hangman():
         L7 = ["\n|"]
         L8 = ["\n|"]
         L9 = ["\n--- "]
-        lettres = []
-        for i in self.mot:
-            lettres += [" _"]
         
-
         # check if letter is right:
-        for rights in self.guess()[0]:
+        for rights in self.guesses[0]:
             for index, letter in enumerate(self.mot):
-                if rights == letter and lettres[index] == ' _':  # CALCULER L'INDEX
-                    lettres[index] = f" {rights}"
-                
-        
+                if rights == letter and self.lettres[index] == ' _':  # CALCULER L'INDEX
+                    self.lettres[index] = f" {rights}"
 
         # check if letter is wrong:
-        if len(self.guess()[1]) == 1:
+        if len(self.guesses[1]) >= 1:
             L3 += '    O'
 
-        if len(self.guess()[1]) == 2:
+        if len(self.guesses[1]) >= 2:
             L4 += '   -'
 
-        if len(self.guess()[1]) == 3:
+        if len(self.guesses[1]) >= 3:
             L4 += '|'
 
-        if len(self.guess()[1]) == 4:
+        if len(self.guesses[1]) >= 4:
             L4 += '-'
 
-        if len(self.guess()[1]) == 5:
+        if len(self.guesses[1]) >= 5:
             L5 += '   / '
 
-        if len(self.guess()[1]) == 6:
+        if len(self.guesses[1]) == 6:
             L5 += "\\"
 
-        hang = L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 + lettres
+        hang = L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 + self.lettres
         return ''.join(hang)
-
-
-    #def draw(self):
-        
-
-    #    L1 = " ----\n"
-    #    L2 = "|    |\n"
-    #    L3 = "|"
-    #    L4 = "\n|"
-    #    L5 = "\n|"
-    #    L6 = "\n|"
-    #    L7 = "\n|"
-    #    L8 = "\n|"
-    #    L9 = "\n---"
-    #    print(L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9)
 
 
     def guess(self):
@@ -87,40 +70,50 @@ class hangman():
             # bon ou pas bon?
             # que faire si reponse pas bonne?
         # doit changer la forme de dessinerHANG() // ajouter lettre
-        essai = input("Pick a letter or make a guess: \n")            
-        guesses = [[''],['']]  # right 0 and wrong 1 guesses
+
+
+        essai = input("Pick a letter or make a guess: \n")         
+        #guesses = [[],[]]  # right 0 and wrong 1 guesses
 
 
         # if the guess is not 1 letter or right amount of letters:
-        #if not isinstance(essai, str) or len(essai) != 1 or len(essai) != len(self.mot):
-         #   print("Must be 1 letter or guess a word with the same amount of letters")
-          #  self.guess()
+        if not isinstance(essai, str) or (len(essai) != 1 and len(essai) != len(self.mot)):
+            print("\nMust be 1 letter or guess a word with the same amount of letters\n")
+            self.guess()
        
-       
+        essai.lower()
+
         #if he guesses a word:
-        if len(essai) == len(self.mot) and essai == self.mot:
-            raise GameError("Vous avez gagné la partie!!!")
+        if essai == self.mot:
+            self.lettres = [self.mot]
         
         # if he guesses a letter and letter is right:
         elif len(essai) == 1 and essai in self.mot:
             # remplacer le tiret de la lettre par la lettre devinée
-            guesses[0] += essai
-            pass
+            self.guesses[0] += essai
+            
         
         # if he guesses a letter and letter is wrong:
         elif len(essai) == 1 and essai not in self.mot:
             # ajouter un membre du bonhomme pendu
-            guesses[1] += essai
-            pass
+            self.guesses[1] += essai
+            
         
-        return guesses
 
 
     def gagner(self):
         #lorsque toute les lettres sont les bonnes
-        pass
+        if ' _' not in self.lettres:
+            return True
 
+    
+    def perdre(self):
+        if len(self.guesses[1]) == 6:
+            return True
+        
 
+        
+        
 # ----
 #|    |
 #|    O
@@ -144,4 +137,5 @@ class hangman():
 #   -|-
 #   / \
 
-test = hangman("allogyujhv")
+#test = hangman("hangman")
+#print(test)
